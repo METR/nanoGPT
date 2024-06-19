3806c0c8b7dda7a363ffb05b72b2946e6859726d
Tao Lin <taoroalin@gmail.com>
2024-06-18 19:24:56 -0700

    cmt

--- model.py
+++ model.py
@@ -125,7 +125,7 @@ class GPT(nn.Module):
         self.config = config
 
         self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wte = nn.Embedding(config.embedding_adapter_dim or config.vocab_size, config.n_embd),
             wpe = nn.Embedding(config.block_size, config.n_embd),
             drop = nn.Dropout(config.dropout),
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
