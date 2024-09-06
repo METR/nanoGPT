d2c2201719e4718476d3962962dfed44566a6a93
Sami Jawhar <sami@metr.org>
2024-09-06 16:27:09 +0000

    Wandb offline by default

--- train.py
+++ train.py
@@ -43,6 +43,7 @@ init_from = "scratch"  # 'scratch' or 'resume' or 'gpt2*'
 wandb_log = True  # disabled by default
 wandb_project = "small_scaling_law"
 wandb_run_name = "wandb_run_name"
+wandb_offline = True
 # data
 dataset = "openwebtext"
 gradient_accumulation_steps = 1  # used to simulate larger batch sizes
@@ -287,6 +288,10 @@ def get_lr(it):
 # logging
 if wandb_log and master_process:
     import wandb
+
+    if wandb_offline:
+        os.environ["WANDB_MODE"] = "offline"
+    os.environ["WANDB_SILENT"] = "true"
 
     wandb.init(project=wandb_project, name=wandb_run_name, config=config)
 
